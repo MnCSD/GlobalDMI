b8e473a152dbc7a27af059ee854af30c659ca907
MnCSD <mnikolopoylos@gmail.com>
2024-01-04 16:49:25 +0200

    changed apos

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -106,7 +106,7 @@ export default function Login() {
 
         <div>
           <span className="text-gray-500">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/register" className="text-blue-400">
               Register
             </Link>
