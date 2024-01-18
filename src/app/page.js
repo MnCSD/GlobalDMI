9f0c52111319818787bcdb27dc9f42e897754b50
MnCSD <mnikolopoylos@gmail.com>
2024-01-18 11:54:25 +0200

    changed home screen

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,9 @@ export default function Home() {
   const router = useRouter();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true)
+  const [countriesData, setCountriesData] = useState([])
+  const [selectedImage, setSelectedImage] = useState("")
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -35,19 +38,25 @@ export default function Home() {
 
     setData(imagesData);
 
+    const uniqueCountries = Array.from(new Set(imagesData.map(item => item.country)));
+
+    // Creating an object to store arrays of items for each country
+    
+    const countriesDataByEach = uniqueCountries.map(country => {
+      return {
+        country: country,
+        items: imagesData.filter(item => item.country === country)
+      };
+    });
+
+    setCountriesData(countriesDataByEach)
+
     setLoading(false)
   };
 
   useEffect(() => {
     getData();
   }, []);
-
-
-  // if(!user) return (
-  //   <div className="flex items-center justify-center text-center text-2xl">
-  //     You have to be verified to access this page.
-  //   </div>
-  // )
 
   return (
     <main className=" min-h-screen bg-[#0F0F0F]">
@@ -85,10 +94,18 @@ export default function Home() {
             <div className="flex items-center justify-center mt-[150px]">
               <ClipLoader color="#3B82F6" size={40}/>
             </div>
-          ) : ( <div className="flex items-center justify-center flex-wrap md:gap-x-10 mt-10 p-10 gap-y-10">
+          ) : ( 
+          
+          <div>
+
+          
+          <div className="flex items-center justify-center flex-wrap md:gap-x-10 mt-10 p-10 gap-y-10">
           {data.map((item) => (
-            <div className="border border-blue-500 relative rounded-lg" key={item.url}>
-              <img src={item.url} alt="" className="w-[300px] h-[200px] object-cover rounded-lg" />
+            <div className="border border-blue-500 relative rounded-lg cursor-pointer" key={item.url} onClick={() => [
+              setSelectedImage(item.url),
+              setOpen(!open)
+            ]}>
+              <img src={item.url} alt="" className="w-[300px] h-[200px] object-cover rounded-lg cursor-pointer" />
   
               <div className="absolute bg-white/50 w-[100%] bottom-0 left-0">
                 <h3 className="text-center font-semibold text-xl">{item.name}</h3>
@@ -106,9 +123,62 @@ export default function Home() {
               </div>
             </div>
           ))}
-        </div>)}
+
+
+         
+
           
-     
+            
+
+            </div>
+        </div>)}
+
+        <div className="mt-10">
+
+        
+        {countriesData.map((country, idx) => (
+              <div className="md:pl-14 " key={idx}>
+                <span className="text-white">Photos from <span className="text-white font-semibold">{country.country}</span> </span>
+                <div className="flex items-center justify-center flex-wrap md:gap-x-10 mt-10 p-10 gap-y-10">
+                {country.items.map((item, idx) => (
+                  
+                    <div className="border border-blue-500 relative rounded-lg" key={item.url}>
+                    <img src={item.url} alt="" className="w-[300px] h-[200px] object-cover rounded-lg" />
+        
+                    <div className="absolute bg-white/50 w-[100%] bottom-0 left-0">
+                      <h3 className="text-center font-semibold text-xl">{item.name}</h3>
+                      <p className="text-center font-semibold text-sm">
+                        {item.description}
+                      </p>
+        
+                      <div className="text-center">
+                        <span>Location: {item.address.substring(9)}</span>
+                      </div>
+        
+                      <div className="flex items-center justify-center">
+                        <span>Date: {item.date}</span>
+                      </div>
+                    </div>
+                  </div>
+                  
+                ))}
+                </div>
+              </div>
+            ))}        
+      </div>
+
+
+      {open && (
+        <div className="fixed top-0 w-screen h-screen left-0 bg-black bg-opacity-60 flex items-center justify-center">
+          
+        
+             <img src={selectedImage} alt="" className="w-[80%] h-[90%] object-cover rounded-lg"/> 
+             <span onClick={() => setOpen(!open)} className="absolute top-2 right-2 bg-gray-500 w-10 h-10 rounded-full cursor-pointer flex items-center justify-center">x</span>
+
+      
+          
+        </div>
+      )}
     </main>
   );
 }
